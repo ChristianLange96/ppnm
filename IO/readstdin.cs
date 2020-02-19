4aca4552a014092ac6f200a6a6cdf74b935ad3a8
ChristianLange96 <chrillelange@gmail.com>
2020-02-19 15:37:33 +0100

    IO a,b is done

--- IO/readstdin.cs
+++ IO/readstdin.cs
@@ -4,13 +4,13 @@ class readcmdline{
 	do{
 		string s = Console.ReadLine();
 		if(s == null)  break;
-		string [] words = s.Split(' '){
-		foreach(var w in words){
-		Console.WriteLine("{0}, {1}, {2}",w, Math.Cos(x), Math.Sin(x));
+		string [] words = s.Split(' ',',','\t');
+		foreach(var word in words){
+		double x = double.Parse(word);
+		Console.WriteLine("{0}, {1}, {2}",x, Math.Cos(x), Math.Sin(x));
 		}
 	}
-	while{true};
-
+	while(true);
 	return 0;
-	}	
+	}
 }
